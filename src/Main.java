0530feaaaceff7c795c1809c15c3f184f10f560b
Carlos Garcia <cgarciapieto@gmail.com>
2018-07-24 15:09:21 -0500

    java-array-classwork2-cgarciapieto.iml

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+
+
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<String> squadList = new ArrayList<String>();
+        squadList.add("bob");
+        squadList.add("John");
+        squadList.add("Kevin");
+        squadList.add("Kevin");
+
+        System.out.println(squadList);
+        squadList.remove(2);
+        System.out.println(squadList.size());
+
+        if(squadList.contains("Kevin")) {
+            System.out.println(squadList.get(2));
+        }
+
     }
 }
